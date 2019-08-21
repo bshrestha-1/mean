1f5605cadffee40738dde8a5908c67bfcbc7344f
bshrestha-1 <bshrestha-1@ou.edu>
2019-08-21 18:24:11 +0000

    created mean and test-mean python files

---
+++ mean.py
@@ -0,0 +1,7 @@
+def mean(num_list):
+    assert isinstance(num_list,list)
+    if len(num_list) ==0:
+        raise Expection("The algebraic mean of an empty list is undefined.") 
+    else:
+        return sum(num_list)/len(num_list)
+
